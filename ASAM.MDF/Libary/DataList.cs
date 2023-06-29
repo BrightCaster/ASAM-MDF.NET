f0a3e18caa5e9ed22c5abf7ae5994deb7fa4015b
Mikhail <mikhail.iukhnovskii.99@mail.ru>
2023-06-29 12:41:36 +0300

    Add losted files

---
+++ ASAM.MDF/Libary/DataList.cs
@@ -0,0 +1,57 @@
+﻿using System.Runtime.InteropServices;
+using System.Windows.Controls;
+
+namespace ASAM.MDF.Libary
+{
+    public class DataList : Block, INext<DataList>
+    {
+        private ulong ptrNextDL;
+        private ulong ptrDataBlockAddress;
+        private ulong ptrDataBlockLen;
+        private DataList nextBlock;
+
+        public ListFlags Flags { get; private set; }
+        public ushort Reserved1 { get; private set; }
+        public uint BlockCount { get; private set; }
+        public long BlockOffset { get; private set; }
+        public DataList(Mdf mdf) : base(mdf)
+        {
+        }
+        public DataList Next
+        {
+            get
+            {
+                if (nextBlock == null && ptrNextDL != 0)
+                    nextBlock = Read(Mdf, ptrNextDL);
+
+                return nextBlock;
+            }
+        }
+
+        public DataBlock DataBlock { get; private set; }
+
+        public static DataList Read(Mdf mdf, ulong position)
+        {
+            mdf.UpdatePosition(position);
+
+            var block = new DataList(mdf);
+            block.Read();
+
+            block.ptrNextDL = mdf.ReadU64();
+            block.ptrDataBlockAddress = mdf.ReadU64();
+            block.Flags = (ListFlags)mdf.ReadU16();
+            block.Reserved1 = mdf.ReadU16();
+            block.BlockCount = mdf.ReadU32();
+
+            if (block.Flags == ListFlags.EqualLength)
+                block.ptrDataBlockLen = mdf.ReadU64();
+            else 
+                block.BlockOffset = mdf.Read64();
+
+            if (block.ptrDataBlockAddress != 0)
+                block.DataBlock = DataBlock.Read(mdf, block.ptrDataBlockAddress);
+
+            return block;
+        }
+    }
+}
---
+++ ASAM.MDF/Libary/DataListCollection.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ASAM.MDF.Libary
+{
+    public class DataListCollection : IList<DataList>
+    {
+        private List<DataList> items = new List<DataList>();
+        public DataListCollection(Mdf mdf, DataGroupBlock dataGroupBlock)
+        {
+            if (mdf == null)
+                throw new ArgumentNullException("mdf");
+
+            Mdf = mdf;
+            DataGroupBlock = dataGroupBlock;
+        }
+
+        public Mdf Mdf { get; private set; }
+        public DataGroupBlock DataGroupBlock { get; internal set; }
+        public int Count
+        {
+            get { return items.Count; }
+        }
+        public bool IsReadOnly
+        {
+            get { return false; }
+        }
+
+        public DataList this[int index]
+        {
+            get { return this.items[index]; }
+            set { throw new NotImplementedException(); }
+        }
+
+        internal void Read(DataList datalistBlock)
+        {
+            items = Common.BuildBlockList(null, datalistBlock);
+        }
+        
+
+        public int IndexOf(DataList item)
+        {
+            return items.IndexOf(item);
+        }
+        public void Insert(int index, DataList item)
+        {
+            items.Insert(index, item);
+        }
+        public void RemoveAt(int index)
+        {
+            items.RemoveAt(index);
+        }
+        public void Add(DataList item)
+        {
+            items.Add(item);
+        }
+        public void Clear()
+        {
+            items.Clear();
+        }
+        public bool Contains(DataList item)
+        {
+            return items.Contains(item);
+        }
+        public void CopyTo(DataList[] array, int arrayIndex)
+        {
+            items.CopyTo(array, arrayIndex);
+        }
+        public bool Remove(DataList item)
+        {
+            return items.Remove(item);
+        }
+
+        public IEnumerator<DataList> GetEnumerator()
+        {
+            return this.items.GetEnumerator();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
