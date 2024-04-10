4edea274e5b6311942d1bd7c42412c7ede47c1e7
Mikhail <mikhail.iukhnovskii.99@mail.ru>
2024-04-10 09:48:14 +0300

    Create INext.cs

---
+++ ASAM.MDF/Libary/INext.cs
@@ -0,0 +1,15 @@
+﻿namespace ASAM.MDF.Libary
+{
+    public interface INext<T> where T : INext<T>
+    {
+        T Next { get; }
+    }
+    public interface IPrevious<T> where T : IPrevious<T>
+    {
+        T Previous { get; set; }
+    }
+    public interface IParent<T>
+    {
+        T Parent { get; set; }
+    }
+}
