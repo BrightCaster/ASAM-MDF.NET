b5b26ce3781c7b21a65166b82753697e99b41ddc
Mikhail <mikhail.iukhnovskii.99@mail.ru>
2024-04-11 10:53:18 +0300

    .

---
+++ ASAM.MDF/Libary/Extensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ASAM.MDF.Libary
+{
+    internal static class Extensions
+    {
+        /// <summary>
+        /// Copy address data to byte massive
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="block">Block coping</param>
+        /// <param name="pointerData">pointer to change</param>
+        /// <param name="bytes">byte massive for copy</param>
+        internal static void CopyAddress<T>(this T block, PointerAddress pointerData, byte[] bytes) where T : Block
+        {
+            if (pointerData.version < 4)
+            {
+                var newbytes = BitConverter.GetBytes(pointerData.AddressV23);
+                Array.Copy(newbytes, 0, bytes, block.BlockAddress + pointerData.offset, newbytes.Length);
+            }
+            else
+            {
+                var newbytes = BitConverter.GetBytes(pointerData.AddressV4);
+                Array.Copy(newbytes, 0, bytes, block.BlockAddress + pointerData.offset, newbytes.Length);
+            }
+        }
+    }
+}
---
+++ ASAM.MDF/Libary/PointerAddress.cs
@@ -0,0 +1,14 @@
+﻿namespace ASAM.MDF.Libary
+{
+    internal class PointerAddress<T> where T : struct
+    {
+        internal readonly int offset;
+        internal readonly T address;
+
+        internal PointerAddress(T ptr, int offset)
+        {
+            this.address = ptr;
+            this.offset = offset;
+        }
+    }
+}
